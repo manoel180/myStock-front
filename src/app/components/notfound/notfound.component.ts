e8fe28186040590ec704007da36ff523a4a59078
Manoel Neto <manoel180@gmail.com>
2024-07-23 23:22:38 -0400

    feat: add docker

--- src/app/components/notfound/notfound.component.ts
+++ src/app/components/notfound/notfound.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
-  styleUrls: ['./notfound.component.css']
+  styleUrls: ['./notfound.component.css'],
+  standalone: true,
+  imports: [ ButtonModule, RippleModule, RouterLink]
 })
 export class NotfoundComponent implements OnInit {
 
